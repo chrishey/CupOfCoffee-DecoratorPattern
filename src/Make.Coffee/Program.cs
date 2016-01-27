3f7a28904e1bc531e0743506991ea93563f201e0
chrishey <chrishey87@hotmail.com>
2016-01-27 13:34:42 +0000

    Implement first step class of filling the kettle

--- src/Make.Coffee/Program.cs
+++ src/Make.Coffee/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Make.Coffee
+﻿using System;
+
+namespace Make.Coffee
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var brewUp = new BrewUp(new FillKettle(null));
+
+            brewUp.CuppaFather();
+
+            Console.ReadKey();
         }
     }
 }
